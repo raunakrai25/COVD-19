b3e830cf975d936268555fbdbd38b34b8b30c350
raunakrai25 <62797086+raunakrai25@users.noreply.github.com>
2020-03-28 19:25:21 +0530

    Create I.cpp

---
+++ I.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    
+    for (a=0;a<=4;a++)
+    {
+        cout<<"*  ";
+    
+    }
+        for (b=0;b<=3;b++)
+        {
+            cout<<"\n      *\n";
+       }
+         
+    for (a=0;a<=4;a++)
+    {
+        cout<<"*  ";
+    }
+
+    return 0;
+}
